dd8e245b11adcc0c99d5de9f770e69d5103cf5a7
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-04-17 20:27:03 +0200

    Posts are being displayed

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 console.log("JS kører");
 
@@ -6,25 +6,44 @@ window.addEventListener("load", initApp);
 
 const endpoint = "https://api-project-pgj-2023-default-rtdb.firebaseio.com/";
 
-function initApp() {
-    getPosts();
+async function initApp() {
+  const posts = await getPosts();
+  displayPosts(posts);
 }
 
 async function getPosts() {
-    const response = await fetch(`${endpoint}/posts.json`);
-    const data = await response.json();
-    const posts = preparePostData(data);
-    console.log(posts);
-    return posts
+  const response = await fetch(`${endpoint}/posts.json`);
+  const data = await response.json();
+  const posts = preparePostData(data);
+  console.log(posts);
+  return posts;
 }
 
 function preparePostData(dataObject) {
-    console.log(dataObject);
-    const postArray = [];
-    for (const key in dataObject) {
-        const post = dataObject[key];
-        post.id = key;
-        postArray.push(post);
-    }
-    return postArray
+  const postArray = [];
+  for (const key in dataObject) {
+    const post = dataObject[key];
+    post.id = key;
+    postArray.push(post);
+  }
+  return postArray;
+}
+
+function displayPosts(posts) {
+  const container = document.querySelector("#posts");
+
+  container.innerHTML = "";
+  for (const post of posts) {
+    const html =
+      /*html*/
+      `<table>
+        <tr>
+          <td>${post.id}</td>
+          <td><img src="${post.image}"></td>
+          <td>${post.title}</td>
+        </tr>
+     `;
+
+    container.insertAdjacentHTML("beforeend", html);
+  }
 }
